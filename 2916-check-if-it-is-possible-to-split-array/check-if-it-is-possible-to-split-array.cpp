7dd4d9d8c91aa654ba0e0483fc7bc157f2286c35
Deepak Nishad <118269722+deepak040302@users.noreply.github.com>
2023-08-11 23:54:46 +0530

    Time: 84 ms (16.47%) | Memory: 26.6 MB (9.80%) - LeetSync

---
+++ 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    bool solve(int i,int j,vector<int>&nums,int m){
+        if(i>j) return false;
+        if(j-i+1==2) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int sum=0;
+        for(int k=i;k<=j;k++) sum+=nums[k];
+
+        int a = false, b= false;
+        if(sum-nums[i] >= m){
+            a = solve(i+1,j,nums,m);
+        }
+        if(sum-nums[j] >= m){
+            b = solve(i,j-1,nums,m);
+        }
+
+        return dp[i][j] = a or b;
+    }
+
+    bool canSplitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        dp.resize(n,vector<int>(n,-1));
+        if(n==1 or n==2) return true;
+        return solve(0,n-1,nums,m);
+    }
+};
+// 2 3 3 2 3
+// 3 3 2 3
+// 3 2 3
+
+
+// 1 5 6 // 
