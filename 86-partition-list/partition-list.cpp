a92a415d93caf25bc84465249880f1e0ddd4dce2
Deepak Nishad <118269722+deepak040302@users.noreply.github.com>
2023-08-15 14:31:40 +0530

    Time: 4 ms (78.23%) | Memory: 10.4 MB (21.22%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -12,19 +12,14 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         ListNode *l1=new ListNode(-1),*l2=new ListNode(-1),*ptr1=l1,*ptr2=l2;
-
         ListNode* tmp=head;
 
         while(tmp!=NULL){
-            if(tmp->val < x){
-                l1->next = new ListNode(tmp->val);
-                l1=l1->next;
-            }else{
-                l2->next = new ListNode(tmp->val);
-                l2=l2->next;
-            }
+            if(tmp->val < x) l1 = l1->next = new ListNode(tmp->val);
+            else l2 = l2->next = new ListNode(tmp->val);
             tmp=tmp->next;
         }
+        
         l1->next = ptr2->next;
         return ptr1->next;
 
