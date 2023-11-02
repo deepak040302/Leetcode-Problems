e66cfa4b5ec57d31e87d4622fe5c340332fcfc2e
Deepak Nishad <118269722+deepak040302@users.noreply.github.com>
2023-11-02 14:37:52 +0530

    Time: 564 ms (63.37%) | Memory: 240.3 MB (34.87%) - LeetSync

--- 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.cpp
+++ 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<vector<int>>dp;
     int dfs(int node,int par,int flg,vector<vector<int>>&adj, vector<int>& coins, int k){
+        if(flg>14) return 0;
         if(dp[node][flg]!=-1) return dp[node][flg];
 
-        int val  = coins[node]/(1<<flg);
+        int val  = coins[node]/(pow(2,flg));
         int a = val - k;
         int b = floor(val/2.0);
 
         for(auto i : adj[node]){
             if(par!=i){
                 a += dfs(i,node,flg,adj,coins,k);
-                if(flg<13) b += dfs(i,node,flg+1,adj,coins,k);
+                b += dfs(i,node,flg+1,adj,coins,k);
             } 
         }
         return dp[node][flg]=max(a,b);
